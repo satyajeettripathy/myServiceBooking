180592d0a167a52e854fb1aab4cc415110149851
satyajeettripathy <satyajeettripathy@gmail.com>
2017-11-18 10:25:13 +0100

    heroku app dependencies-3

--- AutoServiceBookingV02/settings.py
+++ AutoServiceBookingV02/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'AutoServiceBookingV02.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    '''default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'autoservicebooking',
         'USER': 'satyajeet',
         'PASSWORD': 'Satyajeet123',
         'HOST': 'localhost',
         'PORT': '3306',
-    }
+    }'''
 }
 
 
