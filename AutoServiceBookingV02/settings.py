318b51f4e5eb2bddf553c15f8fb4189e9b0d44b8
satyajeettripathy <satyajeettripathy@gmail.com>
2017-11-18 10:23:15 +0100

    heroku app dependencies-2

--- AutoServiceBookingV02/settings.py
+++ AutoServiceBookingV02/settings.py
@@ -133,3 +133,9 @@ EMAIL_HOST_PASSWORD = 'Bot_S@ty@j33t'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
